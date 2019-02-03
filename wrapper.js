4f81e2dc5a136f8c4a82bc399b12970fb261c96b
Scott DeMeo <sd3605@nyu.edu>
2019-02-02 20:28:39 -0500

    changed container sharing from environment variables to shared volume where each sandbox instance writes test results / console output to a unique file before supervisor wrapper sends it back to server request

--- wrapper.js
+++ wrapper.js
@@ -1,15 +1,15 @@
 'use strict';
 
+const fs = require('fs');
+const uniqid = require('uniqid');
 const express = require('express');
 const dockerCLI = require('docker-cli-js');
 const DockerOptions = dockerCLI.Options;
 const Docker = dockerCLI.Docker;
 const docker = new Docker();
 
-// Constants
 const PORT = process.env.PORT || 5000;
 
-// App
 const app = express();
 
 app.use(express.json());
@@ -17,21 +17,46 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/code', async (req, res, next) => {
   try {
+    if (req.body.apiKey !== process.env.apiKey) {
+      res.sendStatus(401);
+    }
+    const sandboxId = uniqid();
+    console.log(sandboxId);
+    const { apiKey, ...noApiKey } = req.body;
+    fs.writeFileSync(
+      `/vol/results-${sandboxId}.json`,
+      JSON.stringify(noApiKey)
+    );
     await docker.command(
-      `run --rm --name docker-sandbox -e userCode="${
-        req.body.userCode
-      }" -m 80m --kernel-memory 25m --ulimit nproc=15 --cpus 1 bonbonbon/docker-sandbox:latest`,
+      `run --rm --name docker-sandbox --volumes-from docker-wrapper -e sandboxId="${sandboxId}" bonbonbon/docker-sandbox:latest`,
       function(err, data) {
         if (err) {
           console.error(err);
-          res.status(400).send('Bad Request: Script execution timed out.');
+          // res.status(400).send('Bad Request: Script execution timed out.');
+          res
+            .status(400)
+            .sendFile(`/vol/results-${sandboxId}.txt`, null, function(fileErr) {
+              if (fileErr) {
+                next(fileErr);
+              } else {
+                console.log('file sent');
+              }
+              fs.unlinkSync(`/vol/results-${sandboxId}.json`);
+              fs.unlinkSync(`/vol/results-${sandboxId}.txt`);
+            });
         } else {
           console.log(data.containerId);
-          const result = data.containerId
-            .split('> node sandbox.js')[1]
-            .replace('\\n', '');
-          const parsedResult = JSON.parse(result);
-          res.send(parsedResult);
+          res.sendFile(`/vol/results-${sandboxId}.txt`, null, function(
+            fileErr
+          ) {
+            if (fileErr) {
+              next(fileErr);
+            } else {
+              console.log('file sent');
+            }
+            fs.unlinkSync(`/vol/results-${sandboxId}.json`);
+            fs.unlinkSync(`/vol/results-${sandboxId}.txt`);
+          });
         }
       }
     );
@@ -43,9 +68,7 @@ app.post('/api/code', async (req, res, next) => {
   }
 });
 
-app.get('/', (req, res, next) => {
-  res.send('docker is running!');
-});
+app.get('/', (req, res, next) => {});
 
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
